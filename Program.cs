66802f6838a5fc44adc697d65bc0804288eeb672
Intermetheus <93767470+Intermetheus@users.noreply.github.com>
2021-11-22 13:59:31 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PixelSouls
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameWorld())
+                game.Run();
+        }
+    }
+}
