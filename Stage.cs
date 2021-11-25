6c79ed65d2a90c41a2c530be86ac13dc57584708
Intermetheus <93767470+Intermetheus@users.noreply.github.com>
2021-11-25 13:59:43 +0100

    Stage + PrototypePlayground

---
+++ Stage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixelSouls
+{
+     class Stage
+    {
+        public static int currentStage;
+        public static Rectangle worldSize;
+
+        public static void LoadLevel(string levelName)
+        {
+
+        }
+
+        public static void UnloadLevel()
+        {
+
+        }
+    }
+}
