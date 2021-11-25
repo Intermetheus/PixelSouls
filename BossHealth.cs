ec8a0adc9f1b8794105b53d4702914a3f3aa3229
oscarcar2 <93787640+oscarcar2@users.noreply.github.com>
2021-11-25 13:44:18 +0100

    UI and UI subclasses created

---
+++ BossHealth.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixelSouls
+{
+    class BossHealth : UI
+    {
+        public override void Draw()
+        {
+
+        }
+    }
+}
