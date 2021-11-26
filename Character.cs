3df15dc7c9961b10d9307659fccc8d7dfa382f40
oscarcar2 <93787640+oscarcar2@users.noreply.github.com>
2021-11-26 11:57:09 +0100

    Update Character.cs

--- Character.cs
+++ Character.cs
@@ -11,17 +11,36 @@ namespace PixelSouls
         protected int health;
         protected int maxHealth;
         protected bool isAlive;
+        
         protected Vector2 initialPosition;
         protected float rotation;
+
+        protected int windupMS = 200;
+        protected int attackDamage = 25;
+        protected int attackWidth = 100;
+        protected int attackHight = 50;
+
+        public int HealthProp
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
 
         public virtual void Attack()
         {
 
         }
+        public virtual Rectangle AttackHitbox()
+        {
+            return new Rectangle((int)position.X, (int)position.Y, attackWidth, attackHight);
+        }
 
         protected virtual void Move(GameTime gameTime)
         {
+            float _deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+            position += ((velocity * speed) * _deltaTime);
         }
 
         public void Rotate(Vector2 target, Vector2 self)
