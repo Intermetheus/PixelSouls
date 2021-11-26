1bb0e714e2dbcc32ff77cab5353978ddddaf899b
Intermetheus <93767470+Intermetheus@users.noreply.github.com>
2021-11-26 10:46:17 +0100

    Update UI.cs
    Update UI.cs

--- UI.cs
+++ UI.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
@@ -11,9 +12,14 @@ namespace PixelSouls
         protected Texture2D sprite;
         protected Vector2 position;
 
-        public virtual void Draw()
+        public virtual void LoadContent(ContentManager content)
         {
+            //sprite = content.Load<Texture2D>("");
+        }
 
+        public virtual void Draw(SpriteBatch spritebatch)
+        {
+            //spritebatch.Draw(sprite, position, Color.White);
         }
     }
 }
