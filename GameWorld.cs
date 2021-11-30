8e63351c2eff4322a835997cfb42c01a0cf8ceda
plazmacode <93761763+plazmacode@users.noreply.github.com>
2021-11-30 10:58:27 +0100

    aaaaaaaaaaaaaaaaaaaa
    aaaaaaaaaaaaaaaaaaa

--- GameWorld.cs
+++ GameWorld.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace PixelSouls
 {
@@ -92,13 +93,19 @@ namespace PixelSouls
             foreach (GameObject gameObject in removeGameObjects)
             {
                 gameObjects.Remove(gameObject);
-                removeGameObjects.Clear();
+                //removeGameObjects.Clear();
             }
+            removeGameObjects.Clear();
 
             foreach (GameObject gameObject in gameObjects)
             {
                 gameObject.Update(gameTime);
+                foreach (GameObject other in gameObjects)
+                {
+                    gameObject.CheckCollision(other);
+                }
             }
+
             ui.Update(gameTime);
 
             base.Update(gameTime);
