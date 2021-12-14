59bd3df25c02dc79913d7561c8b27a1633015b4d
oscarcar2 <93787640+oscarcar2@users.noreply.github.com>
2021-12-14 11:14:23 +0100

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -6,18 +6,21 @@ using System.Text;
 
 namespace PixelSouls
 {
+    /// <summary>
+    /// Subclass of Character managing all elements of the character that are generic to all enemies.
+    /// </summary>
     abstract public class Enemy : Character
     {
         protected int attackCooldown;   // Total attackCooldown
         protected int attackTime;   // The time when the boss starts attacking. Max = attackCooldown
         protected Vector2 playerTarget;
-        protected int attackTrackingLag;
+        protected int attackTrackingLag;   // Used to prevent an attacking enemy from tracking their target after starting an attack, mimmicing the lock-in mechanic on the player.
         protected int windupTimer;
 
         /// <summary>
-        /// Runs the enemies behaviour method. Updates screenPosition & creates collisionBox Rectangle
+        /// Runs the enemy's behaviour method. Updates screenPosition & collisionBox Rectangle
         /// </summary>
-        /// <param name="gameTime"></param>
+        /// <param name="gameTime">Time reference for running update code at a fixed interval</param>
         public override void Update(GameTime gameTime)
         {
             screenPosition = position - GameWorld.CameraPositionProp;
@@ -30,7 +33,7 @@ namespace PixelSouls
         /// <summary>
         /// Updates the Characters position by adding the product of velocity, speed and deltaTime
         /// </summary>
-        /// <param name="gameTime"></param>
+        /// <param name="gameTime">Time reference for running update code at a fixed interval</param>
         protected override void Move(GameTime gameTime)
         {
             float deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
@@ -43,9 +46,9 @@ namespace PixelSouls
         /// </summary>
         private void Behaviour()
         {
-            if(attackCooldown <= 0)
+            if(attackCooldown <= 0)   // Is this enemy able to attack?
             {
-                if (Vector2.Distance(screenPosition, GameWorld.PlayerProp.Position) < 250 && !isAttacking)
+                if (Vector2.Distance(screenPosition, GameWorld.PlayerProp.Position) < 250 && !isAttacking)   // Is the player close enough that this enemy wants to attack and not already attacking?
                 {
                     isAttacking = true;
 
@@ -53,25 +56,23 @@ namespace PixelSouls
 
                     velocity = Vector2.Zero;
 
-                    if (animState != AnimState.Attack)
-                    {
-                        ChangeAnimationState(AnimState.Attack, attackSprites, origin, 5);
+                    ChangeAnimationState(AnimState.Attack, attackSprites, origin, 5);
 
-                        if (screenPosition.X < GameWorld.PlayerProp.Position.X)
-                        {
-                            facingRight = true;
-                            origin = new Vector2(40, 42);
-                        }
-                        else if (screenPosition.X > GameWorld.PlayerProp.Position.X)
-                        {
-                            facingRight = false;
-                            origin = new Vector2(60, 42);
-                        }
-                        animationLock = true;
+                    if (screenPosition.X < GameWorld.PlayerProp.Position.X)   // Determine what direction to face.
+                    {
+                        facingRight = true;
+                        origin = new Vector2(40, 42);
                     }
+                    else if (screenPosition.X > GameWorld.PlayerProp.Position.X)
+                    {
+                        facingRight = false;
+                        origin = new Vector2(60, 42);
+                    }
+
+                    animationLock = true;
                 }    
 
-                if (isAttacking && windupTimer <= 0)
+                if (isAttacking && windupTimer <= 0)   // Once the windup is complete, make an attack
                 {
                     playerTarget = GameWorld.PlayerProp.TargetedPosition;
                     Attack();
@@ -80,22 +81,22 @@ namespace PixelSouls
                     isAttacking = false;
                     animationLock = false;
                 }
-                else if(isAttacking)
+                else if(isAttacking)   // Count down until windup is complete
                 {
                     windupTimer--;
 
-                    if (windupTimer <= attackTrackingLag)
+                    if (windupTimer <= attackTrackingLag)   // Cease tracking once tracking lag is matched.
                     {
                         GameWorld.PlayerProp.IsTargeted = true;
                     }
                 }
             }
-            else
+            else   // If attacking is on cooldown, reduce remaining cooldown
             {
                 attackCooldown--;
             }
 
-            if (!isAttacking)
+            if (!isAttacking)   // So long as the enemy is not attacking, use Move input to update position
             {
                 velocity = GameWorld.PlayerProp.Position - screenPosition;
                 velocity.Normalize();
